8ba3120337b917c1c6e2b8e98a0e1edfaf4b53f4
val11n1 <76488718+val11n1@users.noreply.github.com>
2022-04-23 12:58:31 +0300

    code 2

--- gitPractice/ViewController.swift
+++ gitPractice/ViewController.swift
@@ -21,6 +21,15 @@ class ViewController: UIViewController {
          code
          code
          */
+        
+        
+        //code 2
+        
+        /*
+         code
+         code
+         code
+         */
     }
 
 
